fe66e25edb77b402f4687980b3305cb60077a155
JDups <116138053+JDups@users.noreply.github.com>
2023-10-11 00:09:38 -0400

    Refactoring of Loader. Added checks for windows or unix filepaths in utils.

--- src/GSV_pole_pipeline/loader.py
+++ src/GSV_pole_pipeline/loader.py
@@ -1,26 +1,32 @@
 from utils import get_pole_id_from_filename
 from glob import glob
+import pandas as pd
+import cv2
 
 class img_fetch:
+
+    SUPPORTED_IMAGE_FORMATS = ["png", "jpg"]
 
     def __init__(self, directory=None):
         self.directory = directory
         
         if self.directory:
-            fl_tp = ["*.png", "*.jpg"]
+            self.add_directory(self.directory)
+
+    def add_directory(self, directory):
+            # fl_tp = ["*.png", "*.jpg"]
+            fl_tp = [f"*.{f}" for f in self.SUPPORTED_IMAGE_FORMATS]
             pole_pics = []
             for fl in fl_tp:
                 pole_pics.extend(glob(directory + fl))
 
             self.pole_pics_df = pd.DataFrame({'pole_fp': pole_pics})
+            self.pole_pics_df['pole_id'] = get_pole_id_from_filename(self.pole_pics_df['pole_fp'])
 
-            # print(self.pole_pics_df['pole_fp'].str.split("\\").str[-1])
 
-            # self.pole_pics_df['pole_id'] = self.pole_pics_df['pole_fp'].str.split("/").str[-1].str.split("_").str[1].astype(int)
-            self.pole_pics_df['pole_id'] = get_pole_id_from_filename(self.pole_pics_df['pole_fp'].str)
-
-    def get_batch(self, id):
-        imgs_fp = [row for row in self.pole_pics_df[self.pole_pics_df["pole_id"]==id]["pole_fp"]]
+    def get_batch(self, idn):
+        idn = str(idn)
+        imgs_fp = [row for row in self.pole_pics_df[self.pole_pics_df["pole_id"]==idn]["pole_fp"]]
         images = [cv2.imread(fp) for fp in imgs_fp]
         images = [cv2.cvtColor(image, cv2.COLOR_BGR2RGB) for image in images]
         return [{"img": img, "fp": fp.split('\\')[-1]} for img, fp in zip(images, imgs_fp)]
--- src/GSV_pole_pipeline/utils.py
+++ src/GSV_pole_pipeline/utils.py
@@ -1,2 +1,11 @@
-def get_pole_id_from_filename(filepath):
-	return filepath.str.split("/").str[-1].str.split("_").str[1].astype(int)
+
+
+def get_pole_id_from_filename(filepath_series):
+    if "/" in filepath_series.iloc[0]:
+        dir_div = "/"
+    if "\\" in filepath_series.iloc[0]:
+        dir_div = "\\"
+
+    return filepath_series.str.split(dir_div) \
+                          .str[-1].str.split("_") \
+                          .str[1] # .astype(int) should be kept as string of numbers I think
