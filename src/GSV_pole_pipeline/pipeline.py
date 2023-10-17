2213533b80e7531fecb7a9e330f4fecef9f33938
JDups <116138053+JDups@users.noreply.github.com>
2023-10-17 01:15:08 -0400

    Add comment outlining plans for Pipeline.py

--- src/GSV_pole_pipeline/pipeline.py
+++ src/GSV_pole_pipeline/pipeline.py
@@ -1,3 +1,16 @@
 class Pipeline:
     def __init__(self, loader=None):
         self.loader = loader
+
+"""
+    General idea will be
+        - Loader object that returns batch of images that are to be processed at a time.
+          Loader could also possibly be the one determine the end condition of the whole 
+          process, with it keeping track internally. With a Pipeline level override.
+        
+        - Process objects that will return masks (or possibly other types of predictions)
+        
+        - Rules/Decison object that will take the the previous steps predictions and make decision.
+          The Pipeline could be given a list of what prediction represent what we want and which represent what we don't want.
+          It could then use that list to cross compare the outputs of multiple process objects
+"""
