09b8970b065db0908ef3d0a3e8b51d3afd920896
JDups <116138053+JDups@users.noreply.github.com>
2023-11-22 20:26:35 -0500

    Add setup file

---
+++ src/GSV_pole_pipeline/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="gsv_pole_pipeline",
+    version="0.0.2",
+    description="A small example package",
+    url="https://github.com/JDups/GSV_pole_pipeline",
+    author="Jonathan Dupuis",
+    author_email="jon.c.dupuis@gmail.com",
+    license="MIT",
+    packages=["gsv_pole_pipeline"],
+    zip_safe=False,
+)
