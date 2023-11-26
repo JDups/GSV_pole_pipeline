d91181664e27dcb91f539aa01fead7fb0a1b0cf1
JDups <116138053+JDups@users.noreply.github.com>
2023-11-26 02:20:46 -0500

    Fix redundant GSV call

--- src/GSV_pole_pipeline/loader.py
+++ src/GSV_pole_pipeline/loader.py
@@ -125,12 +125,9 @@ class GSVFetch(Loader):
         return api_list, api_results
 
     def image_from_GSV(self, link):
-        img = np.array(
-            Image.open(BytesIO(requests.get(link).content))
-        )  # TODO: Comment out this line
-
         if link in self.saved_imgs:
             api_results = self.saved_imgs[link]
+            img = self.saved_imgs[link] 
         else:
             img = np.array(Image.open(BytesIO(requests.get(link).content)))
             self.saved_imgs[link] = img
